e1eeddcb0be8a9e674c917fb9b99fab0b7fd8136
Vijay Anand <vijay10pk@gmail.com>
2023-02-11 19:14:16 -0500

    HWK4

---
+++ HWK4/HWK4/Interfaces/IExpenseAnalysisRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HWK4.Models;
+
+namespace HWK4.Interfaces
+{
+	/// <summary>
+	/// Declaration of all the methods in ExpenseAnalysisRepository
+	/// </summary>
+	public interface IExpenseAnalysisRepository
+	{
+		ICollection<Bill> GetExpenses();
+		Bill GetExpenseById(int id);
+        bool BillExists(int id);
+		bool AddExpense(Bill bill);
+		bool EditExpense( Bill updateBill);
+		bool DeleteExpense(int id);
+		void Analysis();
+
+    }
+}
+
---
+++ HWK4/HWK4/Seed.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using HWK4.Data;
+using HWK4.Models;
+
+namespace HWK4
+{
+	public class Seed
+	{
+		private readonly DataContext dataContext;
+        List<Bill> bills;
+
+        public Seed(DataContext dataContext)
+		{
+			this.dataContext = dataContext;
+		}
+
+        /// <summary>
+        /// This method get the data from the ReadDataFromCsv method and store it in the database
+        /// </summary>
+		public void SeedDataContext()
+		{
+			if (!dataContext.Bill.Any())
+			{
+                bills= new();
+                ReadDataFromCsv();
+
+                dataContext.Bill.AddRange(bills);
+                dataContext.SaveChanges();
+            }
+		}
+
+        /// <summary>
+        /// Method that read data from the csv file.
+        /// </summary>
+        private void ReadDataFromCsv()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("./Data/bills.csv"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        bills.Add(new Bill(DateTime.Parse(line[0]),
+                            double.Parse(line[1]),
+                            line[2]));
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+            //return bills;
+
+        }
+
+    }
+}
+
